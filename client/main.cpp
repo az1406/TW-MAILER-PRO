1c47abf81dff773931502bf2d4987dc5890c8e73
Armin Dervisefendic <armindervisefendic@gmail.com>
2024-11-20 19:34:16 +0100

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -10,108 +10,87 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-
     const size_t BUFFER_SIZE = 1024;
     int client_socket;
     char buffer[BUFFER_SIZE];
     struct sockaddr_in address;
     int size;
-    bool isEntryCorrect;
     bool isLogin = false;
-    bool isSend = false;
-    bool isList = false;
-    bool isMessage = false;
-    bool isDel = false;
-    string hs;
 
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Fehler beim erstellen des Sockets");
+        perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
 
     memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
 
-    // ARGUMENT HANDLING
     if (argc != 3) {
         cout << "Usage: ./twmailer-client <ip> <port>\n";
         exit(EXIT_FAILURE);
-    } else {
-        //checks if given ip-address is viable
-        if (inet_pton(AF_INET, argv[1], &address.sin_addr) == 0) {
-            cout << "Input IP-address is not valid\n";
-            exit(EXIT_FAILURE);
-        }
-
-        try {
-            //checks if port is in suitable range
-            if (stoi(argv[2]) < 1024 || stoi(argv[2]) > 65535) {
-                cout << "Input Port is not in usable port range\n";
-                exit(EXIT_FAILURE);
-            }
-        } catch (invalid_argument&) {
-            cout << "Port was not a number\n";
-            exit(EXIT_FAILURE);
-        }
-
-        address.sin_port = htons(stoi(argv[2]));
     }
 
-    if (connect(client_socket, (struct sockaddr *)&address, sizeof(address)) == -1) {
-        perror("Server konnte nicht erreicht werden");
+    if (inet_pton(AF_INET, argv[1], &address.sin_addr) == 0) {
+        cout << "Invalid IP address\n";
         exit(EXIT_FAILURE);
     }
 
-    printf("Connection with server (%s) established\n", inet_ntoa(address.sin_addr));
-
-    size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
-    switch (size) {
-        case -1:
-            perror("recv error");
-            break;
-        case 0:
-            printf("Server closed remote socket\n");
-            break;
-        default:
-            buffer[size] = '\0';
-            printf("%s", buffer);
+    try {
+        int port = stoi(argv[2]);
+        if (port < 1024 || port > 65535) {
+            cout << "Port must be between 1024 and 65535\n";
+            exit(EXIT_FAILURE);
+        }
+        address.sin_port = htons(port);
+    } catch (invalid_argument &) {
+        cout << "Invalid port\n";
+        exit(EXIT_FAILURE);
     }
 
-    do {
-        printf(">> ");
+    if (connect(client_socket, (struct sockaddr *)&address, sizeof(address)) == -1) {
+        perror("Cannot connect to server");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Connected to server (" << inet_ntoa(address.sin_addr) << ")\n";
+
+    size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+    if (size > 0) {
+        buffer[size] = '\0';
+        cout << buffer;
+    }
+
+    while (true) {
+        cout << ">> ";
         if (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
-            int size = strlen(buffer);
-            if (buffer[size - 2] == '\r' && buffer[size - 1] == '\n') {
-                size -= 2;
-                buffer[size] = 0;
-            } else if (buffer[size - 1] == '\n') {
-                --size;
-                buffer[size] = 0;
-            }
+            int len = strlen(buffer);
+            if (buffer[len - 1] == '\n') buffer[len - 1] = '\0'; // Remove trailing newline
 
             if (strcasecmp(buffer, "quit") == 0) {
                 if (client_socket != -1) {
-                    if (shutdown(client_socket, SHUT_RDWR) == -1) {
-                        perror("shutdown create_socket");
-                    }
-                    if (close(client_socket) == -1) {
-                        perror("close create_socket");
-                    }
+                    shutdown(client_socket, SHUT_RDWR);
+                    close(client_socket);
                 }
                 exit(0);
             } else if (strcasecmp(buffer, "login") == 0) {
-                hs = login();
+                string hs = login();
                 send(client_socket, hs.c_str(), hs.size(), 0);
                 size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
                 buffer[size] = '\0';
-                isEntryCorrect = printLogin(buffer);
-                if (isEntryCorrect) {
-                    isLogin = true;
-                    cout << "Connected\n";
+                if (printLogin(buffer)) isLogin = true;
+            } else if (strcasecmp(buffer, "list") == 0) {
+                if (isLogin) {
+                    string hs = list();
+                    send(client_socket, hs.c_str(), hs.size(), 0);
+                    size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+                    buffer[size] = '\0';
+                    printList(buffer);
+                } else {
+                    cout << "Please login first\n";
                 }
             } else if (strcasecmp(buffer, "send") == 0) {
                 if (isLogin) {
-                    hs = send();
+                    string hs = send();
                     send(client_socket, hs.c_str(), hs.size(), 0);
                     size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
                     buffer[size] = '\0';
@@ -119,9 +98,31 @@ int main(int argc, char **argv) {
                 } else {
                     cout << "Please login first\n";
                 }
+            } else if (strcasecmp(buffer, "read") == 0) {
+                if (isLogin) {
+                    string hs = read();
+                    send(client_socket, hs.c_str(), hs.size(), 0);
+                    size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+                    buffer[size] = '\0';
+                    printMessage(buffer);
+                } else {
+                    cout << "Please login first\n";
+                }
+            } else if (strcasecmp(buffer, "del") == 0) {
+                if (isLogin) {
+                    string hs = del();
+                    send(client_socket, hs.c_str(), hs.size(), 0);
+                    size = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+                    buffer[size] = '\0';
+                    printReply(buffer);
+                } else {
+                    cout << "Please login first\n";
+                }
+            } else {
+                cout << "Unknown command\n";
             }
         }
-    } while (true);
+    }
 
     return 0;
 }
